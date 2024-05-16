462077d7a51697f8df865e3bbf078ed70099701d
Jaime Reyes <jaimereyesrdgz@gmail.com>
2024-05-16 11:46:26 +0100

    update to prod

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as fs from 'fs'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(80)
+  const isDevelopment = process.env.NODE_ENV === 'development'
+  const port = isDevelopment
+    ? process.env.PORT || 3000
+    : process.env.HTTPS_PORT || 443
+
+  let httpsOptions = undefined
+
+  if (!isDevelopment) {
+    httpsOptions = {
+      key: fs.readFileSync('/home/admin/decrypted-server.key'),
+      cert: fs.readFileSync('/home/admin/server.crt'),
+    }
+  }
+
+  const app = await NestFactory.create(AppModule, {
+    ...(httpsOptions && { httpsOptions }),
+  })
+
+  app.enableCors()
+
+  await app.listen(port)
 }
+
 bootstrap()
